459a08307c5fc9aa7d06db59c892844745063e51
xDouglas90 <xdouglas90@gmail.com>
2021-05-24 14:16:52 -0300

    Carousel 2 method to Attest section in jQuery function

--- js/main.js
+++ js/main.js
@@ -37,4 +37,26 @@ jQuery(document).ready(function ($) {
       tCounter: "%curr% de %total%",
     },
   });
+
+  // Owl Carousel 2
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    dots: true,
+    lazyLoad: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 2
+      },
+    },
+  });
 });
